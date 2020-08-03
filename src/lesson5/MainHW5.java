13459dacd034b8d574564df76aac94522117fe5c
Flevertyapp <Flevertyap@gmail.com>
2020-08-03 14:11:25 +0300

    Все сделано

---
+++ src/lesson5/MainHW5.java
@@ -0,0 +1,20 @@
+package lesson5;
+
+public class MainHW5 {
+    public static void main(String[] args) {
+        Staff staff1 = new Staff("Ivan Petrov", "engineer", "IvanP@mail.ru", "+7-654-321-85-74", 40000, 45);
+        staff1.staffInfo();
+
+        Staff [] staffArray = new Staff[5];
+        staffArray[0] = new Staff("Васиссуалий Карпов", "инжинер", "VaKa@mail.ru", "+7-654-321-85-74", 40000, 30);
+        staffArray[1] = new Staff("Владилен Серафимов", "ведущий инжинер", "VlSeP@mail.ru", "+7-654-321-85-75", 45000, 35);
+        staffArray[2] = new Staff("Аполлинарий Путятов", "главный инженер", "ApPu@mail.ru", "+7-654-321-85-76", 50000, 45);
+        staffArray[3] = new Staff("Капитолина Светозарова", "зам. директора", "KaSv@mail.ru", "+7-454-397-19-74", 80000, 21);
+        staffArray[4] = new Staff("Доброжир Ярополков", "директор", "Director@mail.ru", "+7-454-397-19-75", 95000, 61);
+
+        for (int i = 0; i < staffArray.length; i++) {
+            staffArray[i].staffOverForty();
+        }
+
+    }
+}
---
+++ src/lesson5/Staff.java
@@ -0,0 +1,30 @@
+package lesson5;
+
+public class Staff {
+    String name;
+    String position;
+    String email;
+    String phone;
+    int salary;
+    int age;
+
+
+    public Staff(String name, String position, String email, String phone, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email= email;
+        this.phone = phone;
+        this.salary= salary;
+        this.age = age;
+    }
+
+    public void staffInfo() {
+        System.out.printf("Name: %s, Position: %s, Email: %s, Phone number: %s, Salary: %d, Age: %d\n", name, position,email, phone, salary, age);
+    }
+    public void staffOverForty() {
+        if (age> 40){
+            System.out.printf("Name: %s, Position: %s, Email: %s, Phone number: %s, Salary: %d, Age: %d\n", name, position,email, phone, salary, age);
+        }
+
+    }
+}
